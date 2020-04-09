7376d6cbccec5468e3ca89349685c53d8b5fce09
ShippingTycoon <59520303+ShippingTycoon@users.noreply.github.com>
2020-04-09 13:55:44 +1200

    Create roster.py

---
+++ roster.py
@@ -0,0 +1,21 @@
+from cs50 import SQL
+from sys import argv, exit
+
+# check correct number of args
+if len(argv)!= 2:
+    print("Usage: python roster.py house_name")
+    exit(1)
+
+# accessing table
+db = SQL("sqlite:///students.db")
+house = argv[1]
+
+# selecting information from table in correct order into list of dicts
+people = db.execute("SELECT first, middle, last, birth FROM students WHERE house = ? ORDER BY last, first;", house)
+
+# print out names and birth years of ppl in specified house
+for i in range(len(people)):
+    if people[i]['middle'] != None:
+        print(f"{people[i]['first']} {people[i]['middle']} {people[i]['last']}, born {people[i]['birth']}")
+    else:
+        print(f"{people[i]['first']} {people[i]['last']}, born {people[i]['birth']}")
